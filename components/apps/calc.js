ee38e8bfa065e0798eb8edc6e87af34c20622e90
mwangimuringi <mwangimuringi10@gmail.com>
2025-01-19 22:55:43 +0300

    Added blinking cursor animation

--- components/apps/calc.js
+++ components/apps/calc.js
@@ -1,49 +1,19 @@
 ...
-export class Calc extends Component {
-  constructor() {
-    super();
-    this.prevCommands = [];
-    this.commandsIndex = -1;
-    ...
+  componentDidMount() {
+    this.appendTerminalRow();
+    this.startCursor(this.terminalRows - 2);
   }
 
-  checkKey = (e) => {
-    const rowId = $(e.target).data('row-id');
-    const command = $(`#calculator-input-${rowId}`).val().trim();
-
-    if (e.key === 'Enter' && command) {
-      this.prevCommands.push(command);
-      this.commandsIndex = this.prevCommands.length;
-      this.handleCommand(command, rowId);
-    } else if (e.key === 'ArrowUp' || e.key === 'ArrowDown') {
-      this.navigateCommandHistory(e.key === 'ArrowUp', rowId);
-    }
+  startCursor = (id) => {
+    clearInterval(this.cursor);
+    this.cursor = setInterval(() => {
+      const cursor = $(`#cursor-${id}`);
+      cursor.css('visibility', cursor.css('visibility') === 'visible' ? 'hidden' : 'visible');
+    }, 500);
   };
 
-  navigateCommandHistory = (isUp, rowId) => {
-    this.commandsIndex += isUp ? -1 : 1;
-    this.commandsIndex = Math.min(Math.max(this.commandsIndex, -1), this.prevCommands.length);
-
-    const prevCommand = this.commandsIndex < 0 || this.commandsIndex >= this.prevCommands.length
-      ? ''
-      : this.prevCommands[this.commandsIndex];
-
-    $(`#calculator-input-${rowId}`).val(prevCommand);
-    $(`#show-calculator-${rowId}`).text(prevCommand);
+  stopCursor = (id) => {
+    clearInterval(this.cursor);
+    $(`#cursor-${id}`).css({ visibility: 'visible' });
   };
-
-  handleCommand = (command, rowId) => {
-    if (command === 'clear') {
-      this.setState({ terminal: [] });
-      this.appendTerminalRow();
-    } else if (command === 'help') {
-      const result = 'Available Commands: clear, help.';
-      this.showResult(rowId, result);
-    }
-  };
-
-  showResult = (rowId, result) => {
-    $(`#row-calculator-result-${rowId}`).html(result);
-    this.appendTerminalRow();
-  };
-
+...
