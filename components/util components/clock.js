df6394c51dc02b530ae728fe9ef4c13fba0eef18
mwangimuringi <mwangimuringi10@gmail.com>
2025-01-19 23:05:55 +0300

    Enhanced variable naming.

--- components/util components/clock.js
+++ components/util components/clock.js
@@ -3,7 +3,20 @@ import React, { Component } from "react";
 export default class Clock extends Component {
   constructor() {
     super();
-    this.month_list = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    this.month_list = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ];
     this.day_list = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
     this.state = {
       hour_12: true,
@@ -30,6 +43,8 @@ export default class Clock extends Component {
     const date = current_time.getDate();
     const meridiem = hour < 12 ? "AM" : "PM";
 
-    return <span>{`${day} ${month} ${date} ${hour}:${minute} ${meridiem}`}</span>;
+    return (
+      <span>{`${day} ${month} ${date} ${hour}:${minute} ${meridiem}`}</span>
+    );
   }
 }
